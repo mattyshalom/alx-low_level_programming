e40c9cc2b995072806f0725b874755ee8d43cf27
mattyshalom <olusegesunday@yahoo.com>
2022-09-08 15:03:57 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
 
-main()
+/**
+ * main - Entry point
+ *
+ *Return: 0
+ */
+
+main ()
 {
-	printf ("with proper grammer, but the outcome is a piece of art,\n");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
 }
