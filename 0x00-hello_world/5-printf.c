c232e8630f686c020e9fc01568b15ed0d8fab55b
mattyshalom <olusegesunday@yahoo.com>
2022-09-08 15:29:50 -0700

    fixed if

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,10 @@
 /**
  * main - Entry point
  *
- *Return: 0
+ * Retun: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
