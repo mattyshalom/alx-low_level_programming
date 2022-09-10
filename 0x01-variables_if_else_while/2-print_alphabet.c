23a565cb6367f551521c7e313fe2a55460c25d7a
mattyshalom <olusegesunday@yahoo.com>
2022-09-10 05:19:13 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,4 +4,15 @@
  * Description: main - Print out a string to stdout using putchar
  * Return: 0 if successful.
  */
-int main(void){
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+	printf("\n");
+
+	return (0);
+}
