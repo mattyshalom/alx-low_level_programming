e41dbb9762f27e16c0059e60c9c12f078bd51f9c
mattyshalom <olusegesunday@yahoo.com>
2022-09-10 09:01:37 -0700

    combinations of two

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Description: main To print combination two digits.
+ * Return: 0 if run successfully.
+ */
+int main(void)
+{
+	int a, b, c,;
+
+	a = 0;
+
+	while (a < 100)
+	{
+		b = a % 10;
+		c = a / 10;
+
+		if (c < b)
+		{
+
+			putchar(c + '0');
+			putchar(b + '0');
+
+			if (a < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
