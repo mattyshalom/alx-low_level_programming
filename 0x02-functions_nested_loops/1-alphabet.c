1f6996926f8f14c95281a44cf879bcba7d0628d2
mattyshalom <olusegesunday@yahoo.com>
2022-09-14 12:45:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * Description: main to print in lowercase to stdout.
+ * Return: 0 when successful.
+ */
+void print_alphabet(void)
+{
+	char e = 'a';
+
+	while (e <= 'z')
+	{
+		_putchar(e);
+		c++;
+	}
+	_putchar('\n');
+}
