a4d04c570cbec3d26dd1db99db0fe4bfd2452e9f
mattyshalom <olusegesunday@yahoo.com>
2022-09-14 11:57:43 -0700

    re-check

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,16 +2,5 @@
 #define HEADER_FILE
 
 int_putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
-int_print_sign(int n);
-int_abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
 
 #endif
