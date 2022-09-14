128b204490b40f9940925c6ad331c8bab14578b2
mattyshalom <olusegesunday@yahoo.com>
2022-09-13 17:05:55 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * Description: main - print small letter a-z
+ * *Return: 0 for successful.
+ */
+int main(void)
+{
+	for ( char k = 'a'; k <= 'z'; k++ )
+	{
+		_putchar( k );
+	}
+	_putchar('\n');
+
+	return 0;
+}
