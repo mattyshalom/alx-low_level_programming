5db58bda966f36848cbc57140952a0ab4e53874d
mattyshalom <olusegesunday@yahoo.com>
2022-09-15 17:12:54 -0700

    3-print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - prints the numbers from 0 to 9.
  * Description: print and followed by a new line
- * REturn: 0 when successful
+ * Return: 0 when successful
  */
 void print_numbers(void)
 {
