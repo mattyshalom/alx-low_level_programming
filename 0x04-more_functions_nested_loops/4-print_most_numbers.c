1126a668ac188ba86b0472d5d62d310176c79a6a
mattyshalom <olusegesunday@yahoo.com>
2022-09-15 17:23:20 -0700

    ommit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9 
+ * Description: do not print 2 and 4
+ * return: 0 if successful.
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+			_putchar((num % 10) + '0');
+	}
+	_putchar('\n');
+}
