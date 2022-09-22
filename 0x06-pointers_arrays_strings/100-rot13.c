1dc397028170f6640ae12452f0bbb3951e42a5a2
mattyshalom <olusegesunday@yahoo.com>
2022-09-22 03:47:58 -0700

    root it up

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,22 +8,20 @@
  */
 char *rot13(char *s)
 {
-  int count = 0, i;
-
-  char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-  while (*(s + count) != '\0')
-  {
-	  for (i = 0; i < 52; i++)
-	  {
-		  if (*(s + count) == alphabet[i])
-		  {
-			  *(s + count) = rot13[i];
-			  break;
-		  }
-	  }
-	  count++;
-  }
-  return (s);
+	int count = 0, i;
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + count) == alphabet[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
+	return (s);
 }
