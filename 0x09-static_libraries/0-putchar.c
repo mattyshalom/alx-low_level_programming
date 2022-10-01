ee14a786ef7b03783991e9d86505ef27ac5ab6b4
mattyshalom <olusegesunday@yahoo.com>
2022-09-30 17:12:54 -0700

    supper task

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * Description: main - print result to stdout.
+ * Return: 0 when successful.
+ */
+int main(void)
+{
+	char putchar[] = "_putchar";
+	int i = 0;
+
+	while (putchar[i] != '\0')
+	{
+		_putchar(putchar[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
