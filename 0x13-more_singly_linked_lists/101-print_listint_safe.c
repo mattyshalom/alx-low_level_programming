6f1f40ceaff7cecf1be30a418a39e1c085e19fec
mattyshalom <olusegesunday@yahoo.com>
2022-10-25 13:54:08 -0700

    done task

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,7 +6,6 @@
  *
  * Return: number of nodes in the list
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t num = 0;
@@ -16,7 +15,6 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		diff = head - head->next;
 		num++;
-
 		printf("[%p] %d\n", (void *)head, head->n);
 
 		if (diff > 0)
@@ -25,10 +23,10 @@ size_t print_listint_safe(const listint_t *head)
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-
 			break;
 		}
 	}
 
 	return (num);
+
 }
