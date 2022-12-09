44534e452af6e45268d2ba5e74ed36d7c566b505
mattyshalom <olusegesunday@yahoo.com>
2022-12-08 17:36:13 -0800

    a function that deletes the node at index index of a dlistint_t linked list.

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,91 +1,54 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of nodes in a doubly linked list
- * @h: pointer to the list
+ * delete_dnodeint_at_index - deletes the node at index of a
+ * dlistint_t linked list
  *
- * Return: number of nodes
- */
-
-size_t dlistint_len(const dlistint_t *h)
-{
-
-	size_t nodes = 0;
-
-	if (!h)
-
-		return (0);
-
-	while (h)
-
-	{
-
-		nodes++;
-
-		h = h->next;
-
-	}
-
-	return (nodes);
-}
-
-/**
- * delete_dnodeint_at_index - deltes a node in a doubly linked list
- * at a given index
- * @head: double pointer to the list
- * @index: index of the node to delete
- *
- * Return: 1 on success, -1 on failure
+ * @head: head of the list
+ * @index: index of the new node
+ * Return: 1 if it succeeded, -1 if it failed
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp = *head;
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
 
-	unsigned int i = 0;
+	h1 = *head;
 
-	if (*head == NULL || dlistint_len(temp) < index + 1)
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
 
-		return (-1);
+	i = 0;
 
-	if (!index)
-
+	while (h1 != NULL)
 	{
+		if (i == index)
+		{
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
 
-		(*head) = temp->next;
+				if (h1->next != NULL)
+					h1->next->prev = h2;
 
-		if (temp->next)
+			}
 
-			temp->next->prev = NULL;
-
-		temp->next = NULL;
-
-		free(temp);
-
-		return (1);
-
-	}
-
-	while (i < index)
-
-	{
-
-		temp = temp->next;
-
+			free(h1);
+			return (1);
+		}
+		h2 = h1;
+		h1 = h1->next;
 		i++;
-
 	}
 
-	temp->prev->next = temp->next;
-
-	if (temp->next)
-
-		temp->next->prev = temp->prev;
-
-	free(temp);
-
-
-
-	return (1);
-
+	return (-1);
 }
